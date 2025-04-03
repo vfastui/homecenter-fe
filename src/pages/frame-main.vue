64e1f276d97ed7e9ec26fbbc9ad4ab113c8431d5
airene <linfanne@gmail.com>
2025-04-03 17:47:08 +0800

    theme

--- src/pages/frame-main.vue
+++ src/pages/frame-main.vue
@@ -1,10 +1,22 @@
 <template>
     <div class="layout">
         <a-layout>
-            <a-layout-header> 111 </a-layout-header>
+            <a-layout-header>
+                <div class="main-content">
+                    <a-menu mode="horizontal" theme="dark" :default-selected-keys="['1']">
+                        <a-menu-item key="0" :style="{ padding: 0, marginRight: '38px' }" disabled>
+                            <div class="logo">HomeCenter</div>
+                        </a-menu-item>
+                        <a-menu-item key="1">Home</a-menu-item>
+                        <a-menu-item key="2">Solution</a-menu-item>
+                        <a-menu-item key="3">Cloud Service</a-menu-item>
+                        <a-menu-item key="4">Cooperation</a-menu-item>
+                    </a-menu>
+                </div>
+            </a-layout-header>
             <a-layout>
                 <a-layout-content>
-                    <div style="padding: 20px">
+                    <div class="main-content">
                         <router-view></router-view>
                     </div>
                 </a-layout-content>
@@ -29,6 +41,7 @@ h2 {
     height: 100%;
 }
 
+/*
 .layout :deep(.arco-layout-sider) {
     width: 206px;
     height: 100%;
@@ -37,9 +50,10 @@ h2 {
 .layout :deep(.arco-layout-has-sider) {
     height: calc(100% - 54px);
 }
+*/
 
 .layout :deep(.arco-layout-header) {
-    height: 54px;
+    height: 59px;
     border-bottom: 1px solid #e5e6eb;
 }
 
@@ -48,12 +62,12 @@ h2 {
     background: #f2f3f5;
 }
 
-a.change:link,
-a.change:visited {
-    color: #000;
-}
-
-a.change:hover {
-    color: #06f;
+.logo {
+    width: 120px;
+    color: #222;
+    height: 30px;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: 500;
 }
 </style>
